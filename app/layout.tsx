faf16db7cc33a644759614ac7a33ba2783213054
Parzival ツ <parzivaleu@gmail.com>
2024-07-12 17:02:22 +0200

    Modify FontSans to Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,13 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 export const metadata: Metadata = {
   title: "OASIS Agency",
   description:
@@ -17,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
