6085342fda5be92f4f0fb01e09b2452e355e3a4e
Parzival ツ <parzivaleu@gmail.com>
2024-07-12 17:00:46 +0200

    Delete Docuement

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 export const metadata: Metadata = {
   title: "OASIS Agency",
   description:
@@ -22,12 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
