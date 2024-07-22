a0e24a950f380755ec8e62adcd2bf391c2cff21e
Parzival ツ <parzivaleu@gmail.com>
2024-07-23 01:27:44 +0200

    Change testimonials for 2 Marquee vertical

--- components/landing/reviews.tsx
+++ components/landing/reviews.tsx
@@ -98,10 +98,24 @@ export function Testimonials() {
             un travail de qualité et une expérience client exceptionnelle. Voici
             quelques retours de mes clients récents.
           </p>
-          <div className="relative flex h-auto min-h-[300px] w-full flex-col items-center justify-center overflow-hidden">
+          <div className="relative flex h-auto min-h-[300px] w-full flex-col items-center justify-center overflow-hidden mt-4">
             <Marquee
               pauseOnHover
-              className="[--duration:50s] flex items-stretch"
+              className="[--duration:30s] sm:[--duration:50s] flex items-stretch"
+            >
+              {reviews &&
+                reviews.map((review: ReviewCardProps) => (
+                  <ReviewCard key={review.id} {...review} />
+                ))}
+              {isLoading &&
+                [...Array(5)].map((_, i) => (
+                  <SkeletonTestimonialCard key={i} />
+                ))}
+            </Marquee>
+            <Marquee
+              reverse
+              pauseOnHover
+              className="[--duration:20s] sm:[--duration:30s] flex items-stretch"
             >
               {reviews &&
                 reviews.map((review: ReviewCardProps) => (
@@ -115,7 +129,7 @@ export function Testimonials() {
             <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
             <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
           </div>
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center mt-4">
             <Button onClick={() => toggleModal()}>Donner un avis</Button>
             <Modal
               title="Ajouter un témoignage"
